9cccc5b1585fd8fdd306e15199df910b8dce29ec
Izzy Matos <israeldmatos@gmail.com>
2016-08-29 14:42:18 -0700

    Add soda count, find soda and sell soda functions

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,12 +7,20 @@ class SodaMachine
   end
 
   def current_inventory_count
+    @sodas.count
   end
 
   def find_soda(soda_brand)
+    @sodas.find {|s| s.brand == soda_brand }
   end
 
   def sell(soda_brand)
+    soda = find_soda(soda_brand)
+    if soda
+      @cash += find_soda(soda_brand).price
+      @sodas.delete(soda)
+    end
+    soda
   end
 
 end
